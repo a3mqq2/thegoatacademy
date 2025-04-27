32c0b0baa9140826f7c50bdaa6821e8ca782fe14
a3mqq2 <aishaaltery03@gmail.com>
2025-04-27 12:50:42 +0200

    updates

--- app/Console/Commands/SendDailyCoursesImage.php
+++ app/Console/Commands/SendDailyCoursesImage.php
@@ -21,7 +21,7 @@ class SendDailyCoursesImage extends Command
         $courses = Course::query()
                   ->whereDate('pre_test_date',   $today)
                   ->orWhereDate('mid_exam_date', $today)
-                  ->orWhereDate('final_exam_date',$today)
+                  ->orWhereDate('final_exam_date', $today)
                   ->get();
 
         $bgB64 = base64_encode(file_get_contents(public_path('images/cource.png')));
@@ -34,27 +34,33 @@ class SendDailyCoursesImage extends Command
 
         $pt   = 255.1;
         $pdf  = Pdf::loadHTML($html)
-                   ->setPaper([0,0,$pt,$pt])
+                   ->setPaper([0, 0, $pt, $pt])
                    ->setOptions([
-                       'dpi'=>96,'isRemoteEnabled'=>true,'isHtml5ParserEnabled'=>true
-                   ])->output();
-        $tmp  = storage_path('app/tmp_card.pdf');
-        file_put_contents($tmp, $pdf);
+                       'dpi' => 96,
+                       'isRemoteEnabled' => true,
+                       'isHtml5ParserEnabled' => true
+                   ])
+                   ->output();
+
+        $tmpPath = storage_path('app/tmp_card.pdf');
+        file_put_contents($tmpPath, $pdf);
 
         $im = new \Imagick();
-        $im->setResolution(300,300);
-        $im->readImage($tmp.'[0]');
+        $im->setResolution(150, 150);
+        $im->readImage($tmpPath.'[0]');
         $im = $im->mergeImageLayers(\Imagick::LAYERMETHOD_FLATTEN);
-        $im->setImageFormat('jpg');
-        $im->setImageCompressionQuality(92);
-        $im->cropThumbnailImage(1020,1020);
+        $im->setImageFormat('jpeg');
+        $im->setImageCompressionQuality(75);
+        $im->cropThumbnailImage(512, 512);
 
-        $filePath = storage_path('app/public/prints/daily_courses_'.now()->format('Ymd_His').'.jpg');
-        $im->writeImage($filePath);
-        unlink($tmp);
+        $fileName = 'prints/daily_courses_' . now()->format('Ymd_His') . '.jpg';
+        Storage::disk('public')->put($fileName, $im);
 
-        $base64 = base64_encode(file_get_contents($filePath));
-        $waapi->sendBase64Image("120363302662559905@g.us", $base64, 'image/jpeg', 'جدول الكورسات اليوم');
+        unlink($tmpPath);
+
+        $imageUrl = asset('storage/' . $fileName);
+
+        $waapi->sendImage("120363302662559905@g.us", $imageUrl, 'جدول الكورسات اليوم');
 
         return self::SUCCESS;
     }
