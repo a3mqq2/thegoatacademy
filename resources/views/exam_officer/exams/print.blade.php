8b8a8190468e8714cd7192bd683a128779941820
a3mqq2 <aishaaltery03@gmail.com>
2025-04-28 21:44:05 +0200

    wer

--- resources/views/exam_officer/exams/print.blade.php
+++ resources/views/exam_officer/exams/print.blade.php
@@ -103,7 +103,7 @@ th, td {
                 Examiner: {{ optional($exam->examiner)->name ?? 'Unassigned' }}<br>
                 Days: {{ $exam->course->days ?? '-' }}
             </div>
-            <div style="text-align: right;">
+            <div style="text-align: right;position: absolute;top:210px;">
                 Time: {{ $exam->time ? \Carbon\Carbon::parse($exam->time)->format('h:i A') : '-' }}<br>
                 Date: {{ $exam->exam_date ? \Carbon\Carbon::parse($exam->exam_date)->format('Y-m-d') : '-' }}
             </div>
